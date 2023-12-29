3931539e18daf3f3781f4fb888b9dc4a021fcbb8
spessein <96380456+unicornizme2911@users.noreply.github.com>
2023-12-29 19:48:07 +0700

    new commit

--- a.py
+++ a.py
@@ -1,19 +1,48 @@
 import cv2
+import numpy as np
 
-def detect_traffic_signs(image):
+def find_contours(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (7,7), 0)
     edges = cv2.Canny(blur, 50, 150)
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        perimeter = cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, 0.001 * perimeter, True)
-        cv2.drawContours(image, [approx], 0, (0,255,0), 2)
-    return image
+    return contours
 
-for i in ['bien-bao-cam-1.png','cam-queo-trai-va-quanh-dau.jpg','cam-xe-may.jpg','di-thang.jpg','qua-duong.png','queo-trai.jpg','stop.png']:
-    image = cv2.imread('./data/'+i)
-    image = detect_traffic_signs(image)
-    cv2.imshow('Detected Signs', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def get_image_contours(image):
+    contours = find_contours(image)
+    height, width = image.shape[:2]
+    blank_image = np.zeros((height,width,3), np.uint8)
+    for i in contours:
+        x, y, w, h = cv2.boundingRect(i)
+        traffic_sign = image[y:y+h, x:x+w]
+        blank_image = traffic_sign
+    return blank_image
+
+def getThreshold(image):
+    # hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # lower_white = np.array([0,0,200], dtype=np.uint8)
+    # upper_white = np.array([180,30,255], dtype=np.uint8)
+    # mask_white = cv2.inRange(hsv, lower_white, upper_white)
+
+    lower_red1 = np.array([0,100,100], dtype=np.uint8)
+    upper_red1 = np.array([10,255,255], dtype=np.uint8)
+
+    lower_red2 = np.array([170,100,100], dtype=np.uint8)
+    upper_red2 = np.array([180,255,255], dtype=np.uint8)
+
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            if (image[i][j] == [255,255,255]).all():
+                continue
+            elif ((image[i][j] >= lower_red1).all() and (image[i][j] <= upper_red1).all()) or (image[i][j] >= lower_red2).all() and (image[i][j] <= upper_red2).all():
+                image[i][j] = [255,0,0]
+            else:
+                image[i][j] = [0,0,0]
+    return image
+image = cv2.imread('./stop.png')
+contours = find_contours(image)
+blank_image = get_image_contours(image)
+new_image = getThreshold(blank_image)
+cv2.imshow('Detected Signs', new_image)
+cv2.waitKey(0)
